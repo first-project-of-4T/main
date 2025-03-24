693ee029b50aa7dad470bcf9b98a7050fbfccd07
1gamseong <rkatjddlsp@gmail.com>
2025-03-25 04:19:25 +0900

    Fouth 수정본

--- navigator_git_4t_app/lib/view/fourth/fouth_image_page.dart
+++ navigator_git_4t_app/lib/view/fourth/fouth_image_page.dart
@@ -7,23 +7,37 @@ class FouthImagePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("자기소개"),
+        centerTitle:true,
+        title: Text("여기도 다크모드로 만들어 봣습니다."),
+        backgroundColor: Colors.black26,
+        titleTextStyle: TextStyle(color: Colors.white)
+
       ),
       body: Center(
         child: Column(
-          children: [
-            Column(
-              children: [Text('date'),
+          children: [Image.asset('imeges/chiken',
+          width: 250,),
+          Text("저는 무슨 사진을 올릴까 카메라를 보니 먹을것 사진이 많더라구요"),
+          Image.asset('imeges/sheeo',
+          width: 250,),
+          Text("어제 밤에햇는데 사실 배고픈걸 참고만들엇습니다."),
+          Image.asset('imeges/cheese',
+          width: 250,),
+          Text("다음에 근처에서 맛잇는걸 같이 먹으러 가실분 계시면 같이가요!"),
+
                 ElevatedButton(onPressed: (){
                 Navigator.pop(context);
                 Navigator.pop(context);
                 },
+                 style: ElevatedButton.styleFrom(
+          backgroundColor: const Color.fromARGB(255, 85, 33, 33),
+                 ),
                  child: Text("Main Screen")),
               ],
             )
-          ],
         ),
-      ),
-    );
+backgroundColor: Colors.black,
+      );
+
   }
 }
