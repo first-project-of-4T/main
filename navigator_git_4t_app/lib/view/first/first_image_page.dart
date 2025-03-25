9a064922266dfee11450a008a617b06753b437db
Heart-fluttery <big5876@gmail.com>
2025-03-25 11:06:36 +0900

    이학현 작업
    개인 작업 수정 및 fourth 오타, 오류 수정

--- navigator_git_4t_app/lib/view/first/first_image_page.dart
+++ navigator_git_4t_app/lib/view/first/first_image_page.dart
@@ -25,7 +25,7 @@ class FirstImagePage extends StatelessWidget {
                   width: 250),
                   Padding(
                     padding: const EdgeInsets.fromLTRB(0, 50, 0, 50),
-                    child: Text("저희 집 강아지 심쿵이 입니다.\n말티즈와 시츄 믹스이고 수컷이며 2022년 9월 18일 생이고\n2022년 11월 22일에 만났습니다.\n이름은 엄마가 쿵이를 처음 봤을 때 심쿵해서 심쿵이로 지어졌으며\n제 성을 따서 이심쿵으로 하자니 마음에 안 들어서 심씨에 이름은 쿵이가 됐습니다.\n제 생의 첫 강아지인데 이보다 사랑스러울 수 없습니다.\n보유 개인기는 돌기, 앉기, 코, 브이, 왼손, 오른손입니다.(갱신중)",
+                    child: Text("저희 집 강아지 심쿵이 입니다.\n말티즈와 시츄 믹스이고 수컷이며 2022년 9월 18일 생이고\n2022년 11월 22일에 만났습니다.\n이름은 엄마가 쿵이를 처음 봤을 때 심쿵해서\n심쿵이로 지어졌으며\n제 성을 따서 이심쿵으로 하자니 마음에 안 들어서\n심씨에 이름은 쿵이가 됐습니다.\n제 생의 첫 강아지인데 이보다 사랑스러울 수 없습니다.\n보유 개인기는 돌기, 앉기, 코, 브이, 왼손, 오른손입니다.\n(갱신중)",
                     style: TextStyle(
                       fontWeight: FontWeight.bold
                     ),),
@@ -35,7 +35,7 @@ class FirstImagePage extends StatelessWidget {
                     child: Image.asset('images/0526.jpg',
                     width: 250,),
                   ),
-                  Text('쿵이는 사람을 굉장히 좋아해서 쳐다보면 누구든 달려가서 애교를 부립니다.',
+                  Text('쿵이는 사람을 굉장히 좋아해서 쳐다보면\n누구든 달려가서 애교를 부립니다.',
                   style: TextStyle(
                     fontWeight: FontWeight.bold
                   ),),
@@ -64,7 +64,7 @@ class FirstImagePage extends StatelessWidget {
                   ),
                   Padding(
                     padding: const EdgeInsets.fromLTRB(0, 0, 0, 50),
-                    child: Text('재미로 만들어본 ai로 재생성한 쿵이 이미지입니다.\n이상으로 자기소"개"를 마치며 쿵이를 보여드릴 기회가 생겨 영광이라 생각합니다.\n저는 귀여운 거, 애니메이션 영화, 게임 좋아합니다. 감사합니다.',
+                    child: Text('재미로 만들어본 ai로 재생성한 쿵이 이미지입니다.\n이상으로 자기소"개"를 마치며 쿵이를 보여드릴\n기회가 생겨 영광이라 생각합니다.\n저는 귀여운 거, 애니메이션 영화, 게임 좋아합니다.\n감사합니다.',
                     style: TextStyle(
                       fontWeight: FontWeight.bold
                     ),),
--- navigator_git_4t_app/lib/view/first/first_page.dart
+++ navigator_git_4t_app/lib/view/first/first_page.dart
@@ -12,31 +12,34 @@ class FirstPage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: const Color.fromARGB(255, 165, 165, 165),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [Column(
-            children: [
-              Text('안녕하세요, 4팀의 첫번째 프로젝트 조장을 맡게 된 이학현입니다. \n 아래 자기소"개"에서 제 사랑스러운 강아지 쿵이를 소"개"해 드리겠습니다.'),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
-                child: ElevatedButton(
-                  onPressed: () => Navigator.pushNamed(context, '/1stimage'),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blueGrey,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)
-                    )
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Column(
+              children: [
+                Text('안녕하세요,\n4팀의 첫번째 프로젝트 조장을 맡게 된 이학현입니다.\n아래 자기소"개"에서 제 사랑스러운 강아지 쿵이를\n소"개"해 드리겠습니다.'),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
+                  child: ElevatedButton(
+                    onPressed: () => Navigator.pushNamed(context, '/1stimage'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blueGrey,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)
+                      )
+                    ),
+                    child: Text('자기소"개"',
+                    style: TextStyle(
+                      color: Colors.white
+                    ),),
                   ),
-                  child: Text('자기소"개"',
-                  style: TextStyle(
-                    color: Colors.white
-                  ),),
                 ),
-              ),
+              ],
+            ),
             ],
           ),
-          ],
         ),
       ),
     );
